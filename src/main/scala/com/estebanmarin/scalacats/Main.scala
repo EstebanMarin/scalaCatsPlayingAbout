277f99c886c1c037ae81893d80a6846a622ed28c
EstebanMarin <estebanmarin002@gmail.com>
2022-10-27 12:37:50 -0500

    working

--- src/main/scala/com/estebanmarin/scalacats/Main.scala
+++ src/main/scala/com/estebanmarin/scalacats/Main.scala
@@ -18,15 +18,25 @@ import cats.kernel.Order
   trait Combinator[A]:
     def combine(x: A, y: A): A
 
+  extension [A](list: List[Option[A]])
+    def combineOptionAll(using Combinator[A]) =
+      list.reduce((a, b) =>
+        (a, b) match
+          case (None, None) => None
+          case (_, None) => a
+          case (None, _) => b
+          case (Some(x), Some(y)) => Some(summon[Combinator[A]].combine(x, y))
+      )
+
   extension [A](list: List[A])
     def combineAll(using Combinator[A]): A =
       list.reduce(summon[Combinator[A]].combine)
 
+  given Combinator[Char] with
+    override def combine(x: Char, y: Char): Char = x
+
   given Combinator[Int] with
     override def combine(x: Int, y: Int): Int = x + y
-
-  given [A: Combinator]: Combinator[Option[A]] with
-    override def combine(x: Option[A], y: Option[A]): Option[A] = ???
 
   case class Person(name: String):
     def greet: String = s"Hi, my $name, nice to meet you"
@@ -36,9 +46,27 @@ import cats.kernel.Order
   val listIntTest = List(1, 2, 3, 4, 5, 6)
   val listStringtes = "testString".toList
 
+  val testListOption = List(Some(4), Some(4), Some(6))
+
+  extension (n: Int) def isPrime = true
+
+  val testSecondExtession: Boolean = 7.isPrime
+
+  sealed abstract class Tree[A]
+  case class Leaf[A](value: A) extends Tree[A]
+  case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+  extension [A](tree: Tree[A]) 
+    def map[B](f: A => B): Tree[B] = 
+      tree match
+        case Leaf(value) => ???
+        case Branch(left, right) => ???
+      
+
   println("hello world")
   println(test.toString())
   println(s"${"Esteban".greetWithPerson}")
-  println(listIntTest.combineAll)
+  println(testListOption.combineOptionAll)
+  println(listStringtes.combineAll)
 
   println("─" * 100)
